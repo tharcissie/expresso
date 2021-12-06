84e58e969931a272113f192b8e478411e7c6c21e
tharcissie <tharcissieidufashe@gmail.com>
2021-12-06 16:57:49 +0200

    created the database

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+
+const apiRouter = require('./api/api');
+app.use('/api', apiRouter);
+
 
 const PORT = process.env.PORT || 4000
-
 app.use(express.static(__dirname + '/public'));
-
 app.listen(PORT, () =>{
     console.log('app running')
 })
+
+module.exports = app;
