4f3ebd9daca9f2596811cf46bcef5936148653c5
tharcissie <tharcissieidufashe@gmail.com>
2021-12-06 17:13:28 +0200

    created crud on menu and menuitem

--- api/api.js
+++ api/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
-const employeesRouter = require('./employees.js');
 const menusRouter = require('./menus.js');
+const employeesRouter = require('./employees.js');
+
 
 apiRouter.use('/menus', menusRouter);
 apiRouter.use('/employees', employeesRouter);
